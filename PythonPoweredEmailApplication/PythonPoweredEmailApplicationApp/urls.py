37e90da04ec6b86eace0af543ebbaca7fefa7d20
BolosBolosHumberGitHub416 <bolosbolosmpjcss@gmail.com>
2025-03-25 18:52:13 -0400

    made some changes to the network programming project

--- PythonPoweredEmailApplication/PythonPoweredEmailApplicationApp/urls.py
+++ PythonPoweredEmailApplication/PythonPoweredEmailApplicationApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-# books_app urls
+# PythonPoweredEmailApplicationApp urls
 urlpatterns = [
+    path('', views.pythonPoweredEmailApplication, name='pythonpoweredemailapplication')
 ]
--- PythonPoweredEmailApplication/PythonPoweredEmailApplicationApp/views.py
+++ PythonPoweredEmailApplication/PythonPoweredEmailApplicationApp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 # Create your views here.
-def pythonPoweredEmailApplication(request,);
+def pythonPoweredEmailApplication(request,):
+    if request.method == 'POST':
+        # senderEmailAddress = request.POST['senderEmailAddress']
+        receiverEmailAddress = request.POST['email']  #receiverEmailAddress
+        # cc = request.POST['cc']
+        # attachFiles = request.POST['attachFiles']
+        subject = request.POST('subject')
+        message = request.POST('message')
+        send = request.POST('send')
+        send_mail(
+            'Send Email', #title
+            message, #message
+            'settings.EMAIL_HOST_USER',
+            ['bolosbolosmpjcss@gmail.com', 'n01297959@humbermail.ca'],
+            fail_silenty=False,
+        )
+    return render(request, 'pythonpoweredemailapplication.html')
